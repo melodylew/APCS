1cf6d268c1c4d0561e4a1ae3b38f8f4c9a47c776
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-09-30 12:20:02 -0400

    tring

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,1 +1,11 @@
+/* Cheese: Melody Lew and Corinna Chen
+APCS
+HW08 -- Refactor Freshie Zero
+2021-09-30 */
 
+
+public class Greet {
+  public static void greet (String){
+    System.out.println("No hablo queso");
+  }
+}
