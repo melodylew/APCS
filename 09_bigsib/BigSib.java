456263fe2dcf685fa44022c2d4ead11d0c652e37
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-10-01 12:19:33 -0400

    Create BigSib.java

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,14 @@
+//Octonauts: Melody Lew and Ducky Ollie/Owen
+//APCS
+//HW09 -- What Are BigSibs Good For?
+//2021-10-01 
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+    public static void BigSib( String str ) {
+      
+}
