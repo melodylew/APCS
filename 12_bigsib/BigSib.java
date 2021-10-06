d934529d39e5ab77d745fd1bdfff1208559c9b0d
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-10-05 21:04:37 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,1 +1,25 @@
+// Silencio -- Melody Lew and Brianna Tieu
+// APCS
+// HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+// 2021-10-05
 
+/*
+DISCO
+0. Since instance variables are non-static, we don't have static in our header.
+
+QCC
+0. What exactly does static mean? 
+1. Why do we need static when we are not using instance variables?
+*/
+
+public class BigSib {
+    String HelloMsg; //instance variable HelloMsg in class BigSib
+
+        public void setHelloMsg(String Msg){ 
+                HelloMsg = Msg;
+        }
+
+    public String greet(String x) {
+        return(HelloMsg + " " + x);
+    }
+}
