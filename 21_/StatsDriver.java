c3e00fbf4f7f6cab90793789d1b7b22b4f191a3c
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-10-20 12:00:36 -0400

    Update StatsDriver.java

--- 21_/StatsDriver.java
+++ 21_/StatsDriver.java
@@ -1,3 +1,36 @@
 public class StatsDriver{
   public static void main(String[] args){
-    
+    System.out.println("Stats test = new Stats()");
+    System.out.println("test.mean(2,4)");
+    System.out.println("...should be 3");
+    System.out.println("test.mean(3,4)");
+    System.out.println("...should be 3");
+    System.out.println("test.mean(4,4)"); 
+    System.out.println("...should be 4");
+    System.out.println("test.mean(2.0,4.0)");
+    System.out.println("...should be 3.0");
+    System.out.println("test.mean(3.0,4.0)");
+    System.out.println("...should be 3.5");
+    System.out.println("test.mean(4.0,4.0)");
+    System.out.println("...should be 4.0");
+    System.out.println("test.max(3,4)");
+    System.out.println("...should be 4");
+    System.out.println("test.max(4,3)");
+    System.out.println("...should be 4");
+    System.out.println("test.max(4,4)");
+    System.out.println("...should be 4");
+    System.out.println("test.max(3.0,4.0)");
+    System.out.println("...should be 4.0");
+    System.out.println("test.max(4.0,3.0)");
+    System.out.println("...should be 4.0");
+    System.out.println("test.max(4.0,4.0)");
+    System.out.println("...should be 4.0");
+    System.out.println("test.geoMean(16,4)");
+    System.out.println("...should be 8");
+    System.out.println("test.geoMean(16,5)");
+    System.out.println("...should be 8"); //still rounds down
+    System.out.println("test.geoMean(16.0,4.0)");
+    System.out.println("...should be 8.0");
+    System.out.println("test.geoMean(16.0,5.0)");
+    System.out.println("...should be 8.9"); //approximation 
+  }
