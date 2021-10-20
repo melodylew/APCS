5bce4094f116e1f2a85e46e3c675068ab5115af3
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-10-20 19:54:40 -0400

    Update Stats.java

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -7,16 +7,19 @@ HW21 -- STAtisTically Speaking...
 
 import java.lang.Math;
 
-public class Stats{
-  public static int mean(int a, int b){
+public class Stats {
+
+  public static int mean(int a, int b) {
     int avg = (a+b)/2;
     return avg;
   }
-  public static double mean(double a, double b){
+
+  public static double mean(double a, double b) {
     double avg = (a+b)/2;
     return avg;
   }
-  public static int max(int a, int b){
+
+  public static int max(int a, int b) {
     if (a >= b){
       return a;
     }
@@ -24,7 +27,8 @@ public class Stats{
       return b;
     }
   }
-  public static double max(double a, double b){
+
+  public static double max(double a, double b) {
     if (a >= b){
       return a;
     }
@@ -32,12 +36,37 @@ public class Stats{
       return b;
     }
   }
-  public static int geoMean(int a, int b){
+
+  public static int geoMean(int a, int b) {
     int mean = Math.sqrt(a*b);
     return mean;
   }
-  public static double geoMean(double a, double b){
+
+  public static double geoMean(double a, double b) {
     double mean = Math.sqrt(a*b);
     return mean;
   }
-}
+
+  public static int max(int a, int b, int c) {
+    
+  }
+
+  public static double max(double a, double b, double c) {
+
+  }
+
+  public static int geoMean(int a, int b, int c) {
+
+  }
+
+  public static double geoMean(double a, double b, double c) {
+
+  }
+
+
+  //main method for testing functionality
+  public static void main( String[] args ) {
+
+  }
+
+}//end class
