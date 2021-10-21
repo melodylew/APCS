da0eb93423d63a5c64d17b054dacffa4bb1e44ba
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-10-20 21:27:47 -0400

    Update Stats.java

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -7,12 +7,20 @@ HW21 -- STAtisTically Speaking...
 
 /*
 DISCO:
-0. You can import Java's math library and use its built in methods such as Math.sqrt and Math.pow. Math.sprt and Math.pow can both be used
+0. For the geoMean methods that use Math.pow, the power has to be written as 1.0/3.0 
+   instead of 1/3, or it returns 1, because the / operator sees two int operands and 
+   returns an int, 0, and anything to the 0 power is 1.
+   Integer division rounds down towards 0, while double division gives a more precise
+   approximation.
+1. You can import Java's math library and use its built in methods such as Math.sqrt and Math.pow. Math.sprt and Math.pow can both be used
 when dealing with exponents.
 
 QCC:
-0. What is considered a "lossy conversion from double to int"? Why do we get this error in int geoMean when all types are integers?
-
+0. How can we make this compilable without using typecasting, specifically for the methods
+   involving means (arithmetic or geometric)?
+1. What is the purpose of the main() method in this class if we're using the driver class,
+   StatsDriver.java, to test the functionality of the code?
+2. What is considered a "lossy conversion from double to int"? Why do we get this error in int geoMean when all types are integers?
 */
 import java.lang.Math;
 
