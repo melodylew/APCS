2f5c468c487989484c791280526288161aff5af9
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-10-31 21:29:54 -0400

    Create helloName.java

---
+++ cd/string-1/helloName.java
@@ -0,0 +1,21 @@
+/*
+Melody Lew
+APCS pd6
+HW28: PPMP
+2021-10-29
+time spent: 0.5 hrs
+*/
+
+//Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+public class helloName{
+  public static void main(String args[]){
+    System.out.println(helloName("Bob")) // "Hello Bob!"
+    System.out.println(helloName("Alice")) // "Hello Alice!"
+    System.out.println(helloName("X")) // "Hello X!"
+   }
+  
+  public String helloName(String name) {
+    String greeting = "Hello " + name + "!";
+    return greeting;
+  }
