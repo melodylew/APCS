d9359eced3d6e0f5c97a243b84b2017f422fa668
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-10-31 21:38:31 -0400

    Create countHi.java

---
+++ cd/string-2/countHi.java
@@ -0,0 +1,29 @@
+/*
+Melody Lew
+APCS pd6
+HW28 -- PPMP
+2021-10-29
+time spent: 0.5 hours
+*/
+
+public class countHi{
+  public static void main(String args[]){
+    System.out.println(countHi("abc hi ho")); // 1
+    System.out.println(countHi("ABChi hi")); // 2
+    System.out.println(countHi("hihi")); // 2
+  }
+  public int countHi(String str) {
+    int count = 0;
+    if (str.length()<=1){
+      count = 0;
+   }
+    else{
+      for (int i = 0; i <= str.length()-1; i ++){
+       if (str.charAt(i)=='h' && str.charAt(i+1)=='i'){
+          count +=1;
+        }
+      }
+   }
+    return count;
+  }
+}
