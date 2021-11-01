73bcec95fc5e87b941bbe6920a5aec18f5ed7dd0
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-10-31 21:32:26 -0400

    Create makeAbba.java

---
+++ cd/string-1/makeAbba.java
@@ -0,0 +1,21 @@
+/*
+Melody Lew
+APCS pd6
+HW28: PPMP
+2021-10-29
+time spent: 0.5 hrs
+*/ 
+
+//Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+public class makeAbba{
+  public static void main(String args[]){
+    System.out.println(makeAbba("Hi", "Bye")); // "HiByeByeHi"
+    System.out.println(makeAbba("Yo", "Alice")); // "YoAliceAliceYo"
+    System.out.println(makeAbba("What", "Up")); // "WhatUpUpWhat"
+    }
+  public String makeAbba(String a, String b) {
+    String s = a+b+b+a;
+    return s;
+  }
+}
