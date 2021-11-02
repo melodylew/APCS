dcd02fb9ef0d933392dcbfe88565b7e0f41a69c4
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-11-02 12:15:12 -0400

    Create Commafier.java

---
+++ 29_cliforeastr/Commafier.java
@@ -0,0 +1,19 @@
+/*
+UnicornHead: Melody Lew, Ariella Katz, Lior Polischouk, Ollie, Tom, Toothless
+APCS pd6
+HW29 -- Which Way Do You Roll?
+2021-11-01
+time spent: 
+*/
+
+/*
+DISCO
+
+QCC
+
+*/
+
+public class Commafier{
+  public static void main(String[] args){
+  }
+  public 
