0accee693fe7156c1b2580abba04281d3782d7e1
melodylew <mlew30@stuy.edu>
2022-01-12 21:09:41 -0500

    add hw 56 girlboss

--- cobo/magpie/activity3/Magpie3.java
+++ cobo/magpie/activity3/Magpie3.java
@@ -1,10 +1,10 @@
 /**
  * A program to carry on conversations with a human user.
- * This version: 
+ * This version:
  * <ul><li>
- *    Uses advanced search for keywords 
- * </li></ul> 
- *    
+ *    Uses advanced search for keywords
+ * </li></ul>
+ *
  * @author Laurie White
  * @version April 2012
  */
@@ -12,7 +12,7 @@ public class Magpie3
 {
 	/**
 	 * Get a default greeting
-	 * 
+	 *
 	 * @return a greeting
 	 */
 	public String getGreeting()
@@ -22,7 +22,7 @@ public class Magpie3
 
 	/**
 	 * Gives a response to a user statement
-	 * 
+	 *
 	 * @param statement
 	 *            the user statement
 	 * @return a response based on the rules given
@@ -44,6 +44,35 @@ public class Magpie3
 				|| findKeyword(statement, "brother") >= 0)
 		{
 			response = "Tell me more about your family.";
+		}
+		else if (findKeyword(statement, "cat") >= 0
+				|| findKeyword(statement, "dog") >= 0)
+		{
+			response = "Tell me more about your pet.";
+		}
+		else if (findKeyword(statement, "Mr.") >= 0)
+		{
+			response = "He sounds like a good teacher.";
+		}
+		else if (findKeyword(statement, "Mrs.") >= 0 || findKeyword(statement, "Mrs.") >= 0)
+		{
+			response = "She sounds like a good teacher.";
+		}
+		else if (findKeyword(statement, "Mx.") >= 0)
+		{
+			response = "They sound like a good teacher.";
+		}
+		else if (statement.trim().length() == 0)
+		{
+			response = "Talk to me.";
+		}
+		else if (findKeyword(statement, "color") >= 0)
+		{
+			response = "I don't have a favorite color.";
+		}
+		else if (findKeyword(statement, "weather") >= 0)
+		{
+			response = "It's quite cold.";
 		}
 		else
 		{
@@ -122,7 +151,7 @@ public class Magpie3
 	 * is not a substring of a longer string (so, for
 	 * example, "I know" does not contain "no"). The search
 	 * begins at the beginning of the string.
-	 * 
+	 *
 	 * @param statement
 	 *            the string to search
 	 * @param goal
@@ -137,7 +166,7 @@ public class Magpie3
 
 	/**
 	 * Pick a default response to use if nothing else fits.
-	 * 
+	 *
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse()
