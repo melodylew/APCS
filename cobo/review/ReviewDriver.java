5673df12ae4b09f1aca7154b43ce13b7bcc738bf
melodylew <mlew30@stuy.edu>
2022-02-10 23:09:28 -0500

    act 1

---
+++ cobo/review/ReviewDriver.java
@@ -0,0 +1,7 @@
+public class ReviewDriver{
+  public static void main(String[] args) {
+    System.out.println(Review.sentimentVal("hello"));
+    System.out.println(Review.sentimentVal("awesome"));
+    System.out.println(Review.sentimentVal("peers"));
+  }
+}
