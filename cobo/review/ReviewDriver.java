9e6742d395cf9598114c72a8a6893204e3ff83c8
Melody Lew <73132847+melodylew@users.noreply.github.com>
2022-02-13 20:12:07 -0500

    Delete ReviewDriver.java

--- cobo/review/ReviewDriver.java
+++
@@ -1,8 +0,0 @@
-public class ReviewDriver{
-  public static void main(String[] args) {
-    System.out.println(Review.sentimentVal("hello"));
-    System.out.println(Review.sentimentVal("awesome"));
-    System.out.println(Review.sentimentVal("peers"));
-    System.out.println(Review.fakeReview("SimpleReview.txt"));
-  }
-}
