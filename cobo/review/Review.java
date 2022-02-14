eef7fd91e3ccf911a285af79fd4a86d8a0a5b3ca
Melody Lew <73132847+melodylew@users.noreply.github.com>
2022-02-14 06:32:20 -0500

    Update Review.java

--- cobo/review/Review.java
+++ cobo/review/Review.java
@@ -196,16 +196,25 @@ public class Review {
 
 //ACTIVITY 3:
   public static String fakeReview(String fileName){
-    String text = textToString(fileName);
-    String newRev = "";
-    while (text.indexOf("*") > -1){
-      String b = text.substring(0,text.indexOf("*"));
-      text = text.substring(text.indexOf("*"));
-      text = text.substring(text.indexOf(" "));
-      newRev += b + randomAdjective();
-    }
-    return newRev + text;
-  }
+   String text = textToString(fileName);
+   String newRev = "";
+   String word = "";
+   while (text.indexOf("*") > -1 && text.indexOf(" ") > -1){
+     String b = text.substring(0, text.indexOf("*"));
+     text = text.substring(text.indexOf("*"));
+     word = text.substring(text.indexOf("*"), text.indexOf(" "));
+     word = removePunctuation(word);
+     newRev += b;
+     if (sentimentVal(word) > 0){
+       newRev = newRev + randomPositiveAdj();
+       text = text.substring(text.indexOf(" "));
+     } else {
+       newRev = newRev + randomNegativeAdj();
+       text = text.substring(text.indexOf(" "));
+     }
+   }
+   return newRev + text;
+ }
   
 // tests code, as instructed by student guide
   public static void main(String[] args){
