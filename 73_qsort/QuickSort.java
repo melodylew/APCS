fb7c37392bc381af483c8c5452a3e4b776667ca8
Melody Lew <73132847+melodylew@users.noreply.github.com>
2022-03-10 07:56:49 -0500

    Update QuickSort.java

--- 73_qsort/QuickSort.java
+++ 73_qsort/QuickSort.java
@@ -1,32 +1,33 @@
-// Minions (Melody Lew, Nora Miller, Nicole Zhou)
-// APCS pd06
+// Team: Minions (Melody Lew, Nora Miller, Nicole Zhou)
+// APCS pd6
 // HW73: All About the About Face
-// 2022-03-09w
-// time spent:  3.0 hrs
+// 2022-03-09
+// time spent: 2 hr
 
-/*
-ALGO:
-Our intended algo~
-0. Have the pivot position be the middle element of the array
-1. Swap until the pivot at this pivot position is in its final resting place
-2. The pivot position will always be the middle element of the array, but the element in the pivot position will change
-2. continue doing this until the value at pivot position is in its final resting place 
+/***
+ * class QuickSort
+ * Implements quicksort algo to sort an array of ints in place
+ *
+ * 1. Summary of QuickSort algorithm:
+ * QSort(arr):
+ * 0) Set the last index as the pvtPos.
+ * 1) Using partition, we found the index where the pivot value is.
+ * 2) Split the array into two sections using the index of the pivot value.
+ * 3) If the array length of the smaller array is 1, does nothing because it is sorted.
+ * 4) Otherwise, repeat steps 0-4.
+ 
+ * 2a. BEST CASE SCENARIO: O(nlogn) which occurs if the pivot position is in the middle and the array can be evenly split. Since we are partitioning 
+  from the middle, if yth is close to the middle then the time complexity is reduced.
+ * 2b. WORST CASE SCENARIO: O(n^2) which occurs if the pivot position is an extreme value and the array is already sorted but cannot be evenly split.
 
-
-BEST CASE SCENARIO: O(nlogn) which occurs if the pivot position is in the middle and the array can be evenly split. Since we are partitioning 
-from the middle, if yth is close to the middle then the time complexity is reduced.
-
-WORST CASE SCENARIO: O(n^2) which occurs if the pivot position is an extreme value and the array is already sorted but cannot be evenly split.
-
-DUPLICATES: Instead of simply splitting the array into two partitions of only less than the pivot and greater than the pivot, have
-a partition that is equal to the pivot.
-
-DISCO:
+ * 3. Approach to handling duplicate values in array:
+ *    No need to worry about it because it will eventually gets sorted using partition.
+ DISCO:
 0) pivot =/= pivot position because the value of pivot may change while pivot position does not
 
 QCC:
 0) How does this work? TT
-*/
+ **/
 
 public class QuickSort{
   //--------------v  HELPER METHODS  v--------------
@@ -65,88 +66,80 @@ public class QuickSort{
   //--------------^  HELPER METHODS  ^--------------
 
 
-  public static int partition( int[] arr, int lower, int upper, int pvtPos){
-    int v = arr[pvtPos];
-    swap( pvtPos, upper, arr);
-    int s = lower;
-    for( int i = lower; i < upper; i++ ) {
-      if ( arr[i] <= v) {
-        swap( i, s, arr );
-        s++;
-      }
-    }
-    swap(s,upper,arr);
-    return s;
-  }//end partition
-
-  public static int fastHelper( int[] arr, int lower, int upper, int pvtPos, int y){
-    //int smaller = partitioner(arr, lower, upper, pvtPos);
-    partition(arr, lower, upper, pvtPos);
-    if (pvtPos == y){// base case
-      return arr[pvtPos]; // if the pivot value is at the pivot position, you're done
-    }
-    if (y == arr.length -1){ // the case where you're looking for the greatest value
-                             // needs to be treated separately because due to typecasting,
-                             // the greatest index will never show up as a pivot.
-      return arr[arr.length - 1];
-    }
-    else {// other cases
-      if (pvtPos > y - 1){ // we need to look more in depth at the lower half
-        int newPvt = (int)((pvtPos + lower)/2);
-        return( fastHelper(arr, lower, pvtPos, newPvt, y));
-      }
-      else { // we need to look at the upper half
-        int newPvt = (int)((pvtPos + upper)/2);
-        return( fastHelper(arr, pvtPos, upper, newPvt, y));
-      }
-    }
+  /**
+   * void qsort(int[])
+   * @param d -- array of ints to be sorted in place
+   */
+  public static void qsort( int[] d ){
+    fastHelper(d, 0, d.length - 1);
   }
 
-  // precond: looking for the yth smallest value in the array
-  // postcond: the yth smallest value of the array (pvt)
-
-  public static int fastSelect(int[] arr, int y){
-    int firstPvt = (int) (0.5 * arr.length);
-    return fastHelper(arr, 0, arr.length-1, firstPvt, y-1);
+  //you may need a helper method...
+  public static void fastHelper( int[] arr, int lower, int upper){
+    if (lower < upper){
+      int newPvt = Partition.partition(arr, lower, upper);
+      fastHelper(arr, lower, newPvt - 1);
+      fastHelper(arr, newPvt + 1, upper);
+    }
   }
 
 
   //main method for testing
   public static void main( String[] args ){
+    //get-it-up-and-running, static test case:
+    int [] arr1 = {7,1,5,12,3};
+    System.out.println("\narr1 init'd to: " );
+    printArr(arr1);
 
-    //init test arrays of magic numbers
-    int[] arr1 = {0,1};
-    int[] arr3 = {1,28,33,4982,37};
-    int[] arr4 = {5,4,17,9000,6,33,44,12};
-    int[] arr5 = {3,3};
+    qsort( arr1 );
+    System.out.println("arr1 after qsort: " );
+    printArr(arr1);
 
-    int[] arr2 = {3,7,9,2,8};
-    partitioner(arr2, 0, 4, 2);
+    // randomly-generated arrays of n distinct vals
+    int[] arrN = new int[10];
+    for( int i = 0; i < arrN.length; i++ )
+    arrN[i] = i;
+
+    System.out.println("\narrN init'd to: " );
+    printArr(arrN);
+
+    shuffle(arrN);
+    System.out.println("arrN post-shuffle: " );
+    printArr(arrN);
+
+    qsort( arrN );
+    System.out.println("arrN after sort: " );
+    printArr(arrN);
+    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+    //get-it-up-and-running, static test case w/ dupes:
+    int [] arr2 = {7,1,5,12,3,7};
+    System.out.println("\narr2 init'd to: " );
     printArr(arr2);
 
-    System.out.print("arr1: ");
-    printArr(arr1);
-    System.out.print(" smallest: ");
-    System.out.println(QuickSort(arr1, 1));
+    qsort( arr2 );
+    System.out.println("arr2 after qsort: " );
+    printArr(arr2);
 
-    System.out.print("arr3: ");
-    printArr(arr3);
-    System.out.print("second smallest: ");
-    System.out.println(QuickSort(arr3, 2));
+    // arrays of randomly generated ints
+    int[] arrMatey = new int[20];
+    for( int i = 0; i < arrMatey.length; i++ )
+    arrMatey[i] = (int)( 48 * Math.random() );
 
-    System.out.print("fourth smallest: ");
-    System.out.println(QuickSort(arr3, 4));
+    System.out.println("\narrMatey init'd to: " );
+    printArr(arrMatey);
 
-    System.out.print("arr4: ");
-    printArr(arr4);
-    System.out.print("third smallest: ");
-    System.out.println(QuickSort(arr4, 3));
+    shuffle(arrMatey);
+    System.out.println("arrMatey post-shuffle: " );
+    printArr(arrMatey);
 
-    System.out.print("seventh smallest: ");
-    System.out.println(QuickSort(arr4, 7));
-
-    System.out.print("eighth smallest: ");
-    System.out.println(QuickSort(arr4, 8));
+    qsort( arrMatey );
+    System.out.println("arrMatey after sort: " );
+    printArr(arrMatey);
+    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
   }//end main
-}
+}//end class QuickSort
