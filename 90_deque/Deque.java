3f95dba32b07cacb8ce66b25692e01c68adab18d
melodylew <mlew30@stuy.edu>
2022-04-12 19:53:19 -0400

    90

--- 90_deque/Deque.java
+++ 90_deque/Deque.java
@@ -2,6 +2,21 @@
 // APCS pd06
 // HW90 -- Swabbing the Deque
 // 2022-04-12
-// time spent:
+// time spent: 0.6 hrs
 
-public
+public interface Deque<Card>{
+  // add an element to the beinning of the queue
+  public void addFirst(Card x);
+  // add an element to the end of the queue
+  public void addLast(Card x);
+  // removes and returns the first element of the queue
+  // public Card removeFirst();
+  // // removes and returns the last element of the queue
+  // public Card removeLast();
+  // // returns the first element of the queue
+  // public Card peekFirst();
+  // // returns the last element of the queue
+  // public Card peekLast();
+
+  // public int size();
+}
